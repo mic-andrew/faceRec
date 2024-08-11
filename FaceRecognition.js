2776af106f17e60523751928d165c1934bd32065
mykmicheals <mykmicheals@gmail.com>
2024-08-11 22:25:44 +0100

    working expo camera

--- FaceRecognition.js
+++ FaceRecognition.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Camera } from "expo-camera";
-import * as FaceDetector from "expo-face-detector";
-import { StatusBar } from "expo-status-bar";
-import FaceRecognition from "./FaceRecognition";
+import { View, Text } from "react-native";
+import React from "react";
 
-const FaceRec = () => {
+const FaceRecognition = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
 
@@ -44,20 +40,4 @@ const FaceRec = () => {
   );
 };
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <FaceRecognition />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  camera: {
-    flex: 1,
-  },
-});
+export default FaceRecognition;
