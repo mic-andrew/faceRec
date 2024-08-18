5dc141026550c1813a015fbaab6e885d986db716
Andrew <mandrew@insight7.io>
2024-08-19 00:48:11 +0100

    deployment commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://192.168.247.243:8000";
-// export const API_BASE_URL="https://schoolnode-fsto.onrender.com";
+// export const API_BASE_URL = "http://192.168.247.243:8000";
+export const API_BASE_URL="https://schoolnode-fsto.onrender.com";
